a8aeb2d8d9e4356d788385e7f8ef8db3ab025b77
Rohitmuz12 <rohitkrmuz00@gmail.com>
2022-08-31 14:58:38 +0530

    Bug fix

--- src/client/modules/my/gamepanel/gamepanel.js
+++ src/client/modules/my/gamepanel/gamepanel.js
@@ -92,8 +92,7 @@ export default class Gamepanel extends LightningElement {
     }
 
     shareGameId(){
-        console.log(this.sharableGameId);
-        var url= 'https://wa.me/?text=Please use the below game Id to connect with your opponent #'+this.sharableGameId;
+        var url= 'https://wa.me/?text=Please use the below game Id to connect with your opponent '+this.sharableGameId;
         window.open(url, '_blank');
     }
 
